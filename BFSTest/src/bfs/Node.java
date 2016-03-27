002e1124c002dbcf6947890fc71029f283fced76
NSmirnov9x <n.smirnov@itigris.ru>
2016-03-27 21:19:31 +0300

    CreateProject

---
+++ BFSTest/src/bfs/Node.java
@@ -0,0 +1,9 @@
+package bfs;
+
+public class Node {
+    Character data;
+
+    public Node(char c) {
+        this.data = c;
+    }
+}
---
+++ BFSTest/src/tests/BfsTests.java
@@ -0,0 +1,6 @@
+package tests;
+
+import org.junit.*;
+
+public class BfsTests {
+}
