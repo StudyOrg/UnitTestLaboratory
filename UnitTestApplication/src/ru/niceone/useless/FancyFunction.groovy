05c3a44d0de053adfadedcebe3eb7ec21def0024
Komarevtsev Roman <hellromul@gmail.com>
2016-04-28 17:44:15 +0300

    Fixes Op.2 No.3 in B-moll

--- UnitTestApplication/src/ru/niceone/useless/FancyFunction.groovy
+++ UnitTestApplication/src/ru/niceone/useless/FancyFunction.groovy
@@ -1,12 +1,33 @@
 package ru.niceone.useless
 
-
+import static ru.niceone.math.Math.*
 
 public class FancyFunction {
     FancyFunction() {}
 
     private static double trigonometry(double x) {
-        ((((((((sec(x) * tan(x)) * sin(x)) ^ 2) ^ 3) / ((cos(x) + (csc(x) + csc(x))) * (cot(x) / sec(x)))) * ((csc(x) - (sin(x) - cot(x))) + tan(x))) - (((sec(x) ^ 2) / sin(x)) + ((sin(x) * sec(x)) * sec(x)))) / cos(x))
+        return (
+                (
+                        (
+                                (
+                                        ((((sec(x) * tan(x)) * sin(x)) ** 2) ** 3) /
+                                        ((cos(x) + (csc(x) + csc(x))) * (cot(x) / sec(x)))
+                                ) *
+                                (
+                                        (csc(x) - (sin(x) - cot(x))) + tan(x)
+                                )
+                        ) -
+                        (
+                                (
+                                        (sec(x) ** 2) / sin(x)
+                                ) +
+                                (
+                                        (sin(x) * sec(x)) * sec(x)
+                                )
+                        )
+                ) /
+                cos(x)
+        )
     }
 
     public static double calc(double x) {
